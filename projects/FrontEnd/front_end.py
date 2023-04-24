9f1caf4e04408242e88368690dc712e8cc594c7e
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-04-24 15:16:17 +0200

    Large update of rsi and refactoring of folders.

---
+++ projects/FrontEnd/front_end.py
@@ -0,0 +1,9 @@
+# """
+# Front-End for various strategies.
+# """
+# from rsi import RSIStrategy
+# k = RSIStrategy('TSLA', '2010-01-01', '2022-04-21')
+# f = k.get_data()
+# l = k.backtest()
+# print(f'Returns: {l:.2%}')
+# # print(l)
