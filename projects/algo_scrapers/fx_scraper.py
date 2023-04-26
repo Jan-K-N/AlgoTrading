6da483d23d8fd3179594f7b13ee0d503c0db50db
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-04-26 15:02:23 +0200

    Update fx_scraper.py

--- projects/algo_scrapers/fx_scraper.py
+++ projects/algo_scrapers/fx_scraper.py
@@ -22,12 +22,28 @@ class ExchangeRatesScraper:
                 data.append([date, currency, rate])
         return pd.DataFrame(data, columns=['Date', 'Currency', 'Exchange Rate'])
 
+    def convert(self, amount, from_currency, to_currency, date):
+        rates = self.scrape()
+        rate = rates.loc[(rates['Currency'] == to_currency) & (rates['Date'] == date), 'Exchange Rate']
+        if not rate.empty:
+            rate = float(rate)
+            converted_amount = amount * rate
+            return converted_amount
+        else:
+            return None
+
 if __name__ == '__main__':
     url = 'https://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesHistoryXML?lang=en'
     scraper = ExchangeRatesScraper(url)
-    k = scraper.scrape()
-    print(k)
+    rates = scraper.scrape()
+    print(rates)
 
+    amount = 100 # e.g. 100 EUR
+    from_currency = 'USD'
+    to_currency = 'EUR'
+    date = '2023-04-25'
+    converted_amount = scraper.convert(amount, from_currency, to_currency, date)
+    print(f'{amount} {from_currency} is {converted_amount:.2f} {to_currency} on {date}')
 
 
 
