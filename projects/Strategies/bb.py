28bf6b1145e77354e5022c248492642579279e3a
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-04-27 13:28:41 +0200

    Update
    Made a larger update for Boillinger bands strategy. Furthermore, I deleted the R_scripts project folder, as we only want to rely on python for now. We will maybe use some R later on.

--- projects/Strategies/bb.py
+++ projects/Strategies/bb.py
@@ -4,15 +4,67 @@ The script contains a backtest of a BB based trading
 strategy.
 """
 
-import yfinance as yf
 import pandas as pd
 import numpy as np
 # pylint: disable=import-error.
 from finance_database import Database
 
 class BollingerBandsStrategy:
+    """
+    Implements a trading strategy based on Bollinger Bands.
 
-    def __init__(self, ticker, start_date, end_date, window=20, dev_factor=2,transaction_cost=0.1):
+    Parameters:
+    -----------
+    ticker : str
+        The stock ticker symbol.
+    start_date : str
+        The start date for data retrieval in the format 'YYYY-MM-DD'.
+    end_date : str
+        The end date for data retrieval in the format 'YYYY-MM-DD'.
+    window : int, optional
+        The size of the moving average window (default is 20).
+    dev_factor : int, optional
+        The number of standard deviations for the band calculation (default is 2).
+    transaction_cost : float, optional
+        The cost of each transaction as a percentage of the transaction value (default is 0.1).
+
+    Methods:
+    --------
+    get_data(self) -> pd.DataFrame:
+        Retrieves price data from a database and calculates the moving average, upper
+        and lower bands for a given ticker within a specific date range.
+        Returns the resulting DataFrame with NaN values dropped.
+
+    backtest(self) -> pd.DataFrame:
+        Simulates a trading strategy using the previously calculated price data, generates buy and
+        sell signals based on the upper and lower bands, and calculates returns based on
+        transaction costs. Returns a DataFrame containing the cumulative returns of the
+        strategy and a boolean variable indicating if there is a buying signal for
+        the given day.
+    """
+
+    # pylint: disable=too-many-arguments.
+    def __init__(self, ticker: str, start_date: str, end_date: str, window: int = 20,
+                 dev_factor: int = 2, transaction_cost: float = 0.1):
+        """
+        Initializes the BollingerBandsStrategy object with the specified parameters.
+
+        Parameters:
+        -----------
+        ticker : str
+            The stock ticker symbol.
+        start_date : str
+            The start date for data retrieval in the format 'YYYY-MM-DD'.
+        end_date : str
+            The end date for data retrieval in the format 'YYYY-MM-DD'.
+        window : int, optional
+            The size of the moving average
+        dev_factor : int, optional
+            The number of standard deviations for the band calculation (default is 2).
+        transaction_cost : float, optional
+            The cost of each transaction as a percentage of the transaction value (default is 0.1).
+
+        """
         self.ticker = ticker
         self.start_date = start_date
         self.end_date = end_date
@@ -44,7 +96,8 @@ class BollingerBandsStrategy:
         # Call the method to retrieve the data
         data = instance.get_data()
         """
-        data = Database.get_price_data(self,start=self.start_date, end=self.end_date,ticker=self.ticker)
+        data = Database.get_price_data(self,start=self.start_date,
+                                       end=self.end_date,ticker=self.ticker)
         data['MA'] = data['Close'].rolling(self.window).mean()
         data['Upper'] = data['MA'] + self.dev_factor * data['Close'].rolling(self.window).std()
         data['Lower'] = data['MA'] - self.dev_factor * data['Close'].rolling(self.window).std()
@@ -52,17 +105,22 @@ class BollingerBandsStrategy:
 
     def backtest(self) -> pd.DataFrame:
         """
-        Simulates a trading strategy using the previously calculated price data, generates buy and sell signals based on the
-        upper and lower bands, and calculates returns based on transaction costs. Returns a DataFrame containing the cumulative
-        returns of the strategy and a boolean variable indicating if there is a buying signal for the given day.
+        Simulates a trading strategy using the previously calculated price data,
+        generates buy and sell signals based on the upper and lower bands, and
+        calculates returns based on transaction costs. Returns a DataFrame
+        containing the cumulative returns of the strategy and a boolean variable
+        indicating if there is a buying signal for the given day.
 
         Returns:
         -------
-        pandas.DataFrame: A DataFrame object containing the cumulative returns and buying signal variable.
+        pandas.DataFrame: A DataFrame object containing the cumulative returns and
+        buying signal variable.
 
         Examples:
         # Initialize an instance of the class with the required parameters
-        instance = BollingerBandsStrategy(start_date='2022-01-01', end_date='2022-01-31', ticker='AAPL', window=20, dev_factor=2, transaction_cost=0.01)
+        instance = BollingerBandsStrategy(start_date='2022-01-01',
+         end_date='2022-01-31', ticker='AAPL', window=20,
+        dev_factor=2, transaction_cost=0.1)
 
         # Call the method to backtest the strategy
         data = instance.backtest()
@@ -71,7 +129,8 @@ class BollingerBandsStrategy:
         self.data.loc[self.data['Close'] < self.data['Lower'], 'Position'] = 1
         self.data.loc[self.data['Close'] > self.data['Upper'], 'Position'] = -1
         self.data['Transaction Cost'] = self.transaction_cost * self.data['Close'].diff().abs()
-        self.data['Returns'] = (self.data['Close'].pct_change() - self.data['Transaction Cost']).shift(1) * self.data['Position']
+        self.data['Returns'] = (self.data['Close'].pct_change() -
+                                self.data['Transaction Cost']).shift(1) * self.data['Position']
         self.data['Cumulative Returns'] = (1 + self.data['Returns']).cumprod()
 
         data = self.data[['Cumulative Returns']]
