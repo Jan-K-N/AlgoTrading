93514c8ab84ee00390b5e90058037435a569692f
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-04-29 14:40:41 +0200

    Update front_end.py

--- projects/FrontEnd/front_end.py
+++ projects/FrontEnd/front_end.py
@@ -1,9 +1,32 @@
-# """
-# Front-End for various strategies.
-# """
-# from rsi import RSIStrategy
-# k = RSIStrategy('TSLA', '2010-01-01', '2022-04-21')
-# f = k.get_data()
-# l = k.backtest()
-# print(f'Returns: {l:.2%}')
-# # print(l)
+"""
+Front-end beta: Basic front end project made
+to invstigate the possibilities with streamlit.
+"""
+import sys
+import pandas as pd
+import streamlit as st
+
+# pylint: disable=import-error
+# pylint: disable=wrong-import-position
+sys.path.insert(1, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/data')
+from finance_database import Database
+
+st.title('AlgoTrading: Stock comparison')
+
+tickers = ('TSLA','AAPL')
+
+dropdown = st.multiselect('Pick your asset',
+                          tickers)
+
+start = st.date_input('Start', value = pd.to_datetime('2021-01-01'))
+end = st.date_input('End', value = pd.to_datetime('today'))
+
+
+instance = Database()
+
+if len(dropdown) > 0:
+    df = instance.get_price_data(start=start, end=end, ticker=dropdown)
+    df = df['Adj Close']
+    st.line_chart(df)
+
+# /Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/FrontEnd/front_end.py
