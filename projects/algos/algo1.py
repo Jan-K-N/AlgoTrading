9d04f8859d7fba49ae494da523f81c2524f2c9c5
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-05-01 16:31:45 +0200

    Updated algo1 and simplified rsi.

--- projects/algos/algo1.py
+++ projects/algos/algo1.py
@@ -3,31 +3,93 @@ Main script for algo1
 """
 
 import pandas as pd
+# pylint: disable=import-error
 from rsi import RSIStrategy
+# pylint: disable=import-error
 from bb import BollingerBandsStrategy
+# pylint: disable=import-error
 from finance_database import Database
 
 
 class Algo1:
-    def __init__(self,ticker: str, start_date=None,end_date=None):
+    """
+    A class representing an algorithm for generating trading signals based on
+    Relative Strength Index (RSI) and Bollinger Bands strategies.
+
+    Attributes:
+    -----------
+    ticker : str
+        The ticker symbol of the financial instrument.
+    start_date : str or None
+        The start date for retrieving price data. If None, the default is None.
+    end_date : str or None
+        The end date for retrieving price data. If None, the default is None.
+    tickers_list : list or None
+        A list of ticker symbols for multiple financial instruments. If None, the default is None.
+
+    Methods:
+    --------
+    rsi() -> pd.Series:
+        Calculates the Relative Strength Index (RSI) for the specified ticker and date range.
+
+    BollingerBands() -> pd.DataFrame:
+        Retrieves Bollinger Bands data for the specified ticker and date range.
+
+    generate_signals() -> pd.DataFrame:
+        Generates buy and sell signals based on RSI and Bollinger Bands strategies.
+
+    Algo1_loop() -> list:
+        Executes the algorithm for multiple tickers and returns a list of signals for each ticker.
+    """
+    def __init__(self,ticker: str, start_date=None,end_date=None, tickers_list=None):
         self.ticker = ticker
         self.start_date = start_date
         self.end_date = end_date
+        self.tickers_list = tickers_list
     def rsi(self)->pd.Series:
-        rsi_instance = RSIStrategy(ticker = self.ticker,start_date=self.start_date,end_date=self.end_date)
+        """
+        Calculates the Relative Strength Index (RSI) for the specified ticker and date range.
+
+        Returns:
+        -------
+        rsi_data : pd.Series
+            Series containing the RSI values.
+        """
+        rsi_instance = RSIStrategy(ticker = self.ticker,
+                                   start_date=self.start_date,
+                                   end_date=self.end_date)
         rsi_data = rsi_instance.get_data()['RSI']
         return rsi_data
-    def BollingerBands(self):
+    def bollinger_bands(self):
+        """
+        Retrieves Bollinger Bands data for the specified ticker and date range.
+
+        Returns:
+        -------
+        bollingerbands_data : pd.DataFrame
+            DataFrame containing the Bollinger Bands data.
+        """
         bollingerbands_instance = BollingerBandsStrategy(ticker = self.ticker,
-                                                         start_date=self.start_date,end_date=self.end_date)
+                                                         start_date=self.start_date,
+                                                         end_date=self.end_date)
         bollingerbands_data = bollingerbands_instance.get_data()
         return bollingerbands_data
 
     def generate_signals(self):
-        data = Database.get_price_data(self,ticker = self.ticker,start=self.start_date,end=self.end_date)['Adj Close']
+        """
+        Generates buy and sell signals based on RSI and Bollinger Bands strategies.
 
-        lower_band = Algo1.BollingerBands(self)['Lower']
-        upper_band = Algo1.BollingerBands(self)['Upper']
+        Returns:
+        -------
+        signals : pd.DataFrame
+            DataFrame containing the buy and sell signals.
+        """
+        data = Database.get_price_data(self,ticker = self.ticker,
+                                       start=self.start_date,
+                                       end=self.end_date)['Adj Close']
+
+        lower_band = Algo1.bollinger_bands(self)['Lower']
+        upper_band = Algo1.bollinger_bands(self)['Upper']
         rsi = Algo1.rsi(self)
 
         buy_signal = (rsi < 30) & (data[1] < lower_band)
@@ -40,15 +102,19 @@ class Algo1:
         sell_signal_list = sell_signal.astype(int).tolist()
         signals[self.ticker + '_Sell'] = sell_signal_list
 
-        k = print("h")
+        return signals
 
+    def algo1_loop(self):
+        """
+        Executes the algorithm for multiple tickers. The function returns a
+        list with the buying/selling signals.
+        """
 
+        signals_list = []
 
-if __name__== '__main__':
-    ticker_code = 'TSLA'
-    k = Algo1(ticker=ticker_code,start_date='2020-02-01',end_date='2023-04-28')
-    f=k.rsi()
-    f1=k.BollingerBands()
-    f2 = k.generate_signals()
+        for ticker1 in self.tickers_list:
+            instance_1 = Algo1(ticker=ticker1, start_date=self.start_date,end_date=self.end_date)
+            signals_1 = instance_1.generate_signals()
+            signals_list.append(signals_1)
 
-
+        return signals_list
