94a8fdf780a46652aeab6f826d35852f8ab0d807
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-05-02 16:27:33 +0200

    Init

--- projects/FrontEnd/front_end.py
+++ projects/FrontEnd/front_end.py
@@ -10,6 +10,8 @@ import streamlit as st
 # pylint: disable=wrong-import-position
 sys.path.insert(1, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/data')
 from finance_database import Database
+sys.path.insert(1, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/algos')
+from algo1 import Algo1
 
 st.title('AlgoTrading: Stock comparison')
 
@@ -22,11 +24,10 @@ start = st.date_input('Start', value = pd.to_datetime('2021-01-01'))
 end = st.date_input('End', value = pd.to_datetime('today'))
 
 
-instance = Database()
+instance = Algo1(ticker = dropdown)
 
 if len(dropdown) > 0:
-    df = instance.get_price_data(start=start, end=end, ticker=dropdown)
-    df = df['Adj Close']
-    st.line_chart(df)
+    df = instance.generate_signals(start=start, end=end, ticker=dropdown)
+    df
 
 # /Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/FrontEnd/front_end.py
