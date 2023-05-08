797e321d8b26a3f0eb5b9798fa510a98ab525fc3
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-05-08 17:02:58 +0200

    S&P500 ticker codes scraper.

---
+++ projects/algo_scrapers/s_and_p_scraper.py
@@ -0,0 +1,72 @@
+"""
+Main script for scraping S&P500 ticker codes from Wikipedia.
+"""
+import logging
+import requests
+from bs4 import BeautifulSoup
+
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class SAndPScraper:
+    """
+    A class for scraping ticker codes from the S&P500.
+
+    Attributes:
+        base_url (str): The base URL of the S&P500.
+        ticker_url (str): The URL of the S&P500 page.
+
+    Methods:
+        __init__(): Initialize the SAndPScraper class.
+        scrape_ticker_codes(): Scrape ticker codes from the S&P500.
+        run_scraper(): Run the ticker code scraping process.
+    """
+    def __init__(self):
+        """
+        Initialize the SAndPScraper class.
+
+        The base URL is set to 'https://en.wikipedia.org',
+        and the ticker URL is set to the S&P500 page URL.
+        """
+        self.base_url = 'https://en.wikipedia.org'
+        self.ticker_url = self.base_url + '/wiki/List_of_S%26P_500_companies'
+
+    def scrape_ticker_codes(self) -> list:
+        """
+        Scrape ticker codes from the S&P500 page.
+
+        Returns:
+            list: A list of ticker codes.
+
+        Raises:
+            requests.exceptions.RequestException: If an error occurs while accessing the website.
+        """
+        try:
+            response = requests.get(self.ticker_url, timeout=10)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            table = soup.find('table', {'class': 'wikitable sortable'})
+            rows = table.find_all('tr')
+
+            ticker_codes = []
+
+            for row in rows[1:]:
+                columns = row.find_all('td')
+                ticker_code = columns[0].text.strip()
+                ticker_codes.append(ticker_code)
+
+            return ticker_codes
+
+        except requests.exceptions.RequestException as exc:
+            logging.error("Error occurred while accessing the website: %s", str(exc))
+            return None
+
+    def run_scraper(self) -> list:
+        """
+        Run the ticker code scraping process.
+
+        Returns:
+            list: A list of ticker codes obtained from the scraping process.
+        """
+        print("Retrieving ticker codes from...:", self.ticker_url)
+        ticker_codes = self.scrape_ticker_codes()
+        return ticker_codes
