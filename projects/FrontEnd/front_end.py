383f620e9bc18b3b9a45e4a8012411ed2a676b65
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-05-29 15:06:00 +0200

    Update front_end.py

--- projects/FrontEnd/front_end.py
+++ projects/FrontEnd/front_end.py
@@ -1,12 +1,15 @@
 """
 Front end dash for algo1. The app is beta.
 """
-
 import sys
+import time
 import dash
 import dash_table
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from datetime import datetime, timedelta
+
 sys.path.insert(0, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects')
 sys.path.insert(1, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/data')
 sys.path.insert(2, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/algos')
@@ -19,7 +22,7 @@ from dax_scraper import DAXScraper
 
 
 # Create the Dash app
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Define the app layout
 app.layout = html.Div(
@@ -27,6 +30,7 @@ app.layout = html.Div(
         html.H1("Algo1 Loop Output"),
         html.Div(
             children=[
+                dbc.Progress(value=0, id='progress-bar', style={'width': '50%', 'margin': 'auto'}),
                 dcc.Dropdown(
                     id='market-dropdown',
                     options=[
@@ -41,25 +45,27 @@ app.layout = html.Div(
                     start_date_placeholder_text='Start Date',
                     end_date_placeholder_text='End Date',
                     min_date_allowed="2015-01-01",
-                    max_date_allowed="2023-05-08",
-                    initial_visible_month="2023-01-09",
-                    start_date="2023-01-09",
-                    end_date="2023-05-03"
-                )
+                    max_date_allowed=(datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d'),
+                    initial_visible_month=datetime.now().strftime('%Y-%m-%d'),
+                    start_date=(datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d'),
+                    end_date=(datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+                ),
+                html.Div(id='out-box')
             ],
             style={"display": "inline-block", "width": "100%"},
-        ),
-        html.Div(id='out-box')
+        )
     ]
 )
 
+
 @app.callback(
-    dash.dependencies.Output('out-box', 'children'),
-    dash.dependencies.Input('market-dropdown', 'value'),
-    dash.dependencies.Input('date-range-picker', 'start_date'),
-    dash.dependencies.Input('date-range-picker', 'end_date')
+    [dash.dependencies.Output('progress-bar', 'value'),
+     dash.dependencies.Output('out-box', 'children')],
+    [dash.dependencies.Input('market-dropdown', 'value'),
+     dash.dependencies.Input('date-range-picker', 'start_date'),
+     dash.dependencies.Input('date-range-picker', 'end_date')]
 )
-def update_out_box(market:str, start_date:str, end_date:str)->html.Div:
+def update_out_box(market:str, start_date:str, end_date:str)->(int, html.Div):
     """
     Update the output box with signals for the chosen market and time period.
 
@@ -76,7 +82,8 @@ def update_out_box(market:str, start_date:str, end_date:str)->html.Div:
 
     Returns:
     -----------
-    html.Div: A div element containing tables with the generated signals for each ticker.
+    (int, html.Div): An integer indicating the progress value of the progress bar and
+                    a div element containing tables with the generated signals for each ticker.
     """
     if market == 'DAX':
         instance_dax = DAXScraper()
@@ -91,7 +98,8 @@ def update_out_box(market:str, start_date:str, end_date:str)->html.Div:
 
     # Check if output_list is empty
     if not output_list:
-        return html.Div(children=["No signals found for the chosen period"])
+        return 0, html.Div(children=["No signals found for the chosen period"])
+
     ticker_tables = [
         html.Div(
             children=[
@@ -124,7 +132,11 @@ def update_out_box(market:str, start_date:str, end_date:str)->html.Div:
         ) for i in range(len(output_list))
     ]
 
-    return ticker_tables
+    # Calculate progress value based on the number of processed tickers
+    progress_value = int(len(output_list) / len(tickers_list) * 100)
+
+    return progress_value, ticker_tables
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
