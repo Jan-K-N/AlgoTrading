eda6d2464f88c580a7bd9275cd1f36814d91ebeb
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-09-02 14:39:29 +0200

    Update algo2.py

--- projects/algos/algo2.py
+++ projects/algos/algo2.py
@@ -3,7 +3,11 @@ Main script for algo2. This algo will find signals in the same way
 as algo1. However, after the signals are found, this algo1
 will do some more analysis.
 """
+import sys
 from algo1 import Algo1
+sys.path.insert(1,'/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/data')
+# from data import finance_database
+import pandas as pd
 
 class Algo2:
     def __init__(self,ticker=None, start_date=None,end_date=None, tickers_list=None):
@@ -20,10 +24,70 @@ class Algo2:
 
         return algo1_output
 
+
     def random_forest(self):
         """
         Function for creating a random forest for the stock return.
+        Input to the rf should be the stock returns from the suggested
+        stocks from algo1. If there is a buy/sell signal for a given date,
+        we pull the return data for the given stock up till that day.
+        Then we run a random forest model to predict the probability
+        of a up/down movement.
 
         Returns:
 
         """
+        instance = Algo2(start_date=self.start_date,
+                         end_date=self.end_date,
+                         tickers_list=self.tickers_list)
+        algo1_signals = instance.run_algo1()
+
+        # Initialize empty lists to store DataFrames for buy and sell signals
+        buy_dataframes = []
+        sell_dataframes = []
+
+        for df in algo1_signals:
+            # Access the "buy" and "sell" columns by name
+            ticker_column = df.iloc[:, 0]
+            buy_column = df.iloc[:, 1]
+            sell_column = df.iloc[:, 2]
+
+            # Create empty DataFrames to store the filtered buy and sell data
+            filtered_buy_df = pd.DataFrame(columns=['Ticker', 'Buy_Date'])
+            filtered_sell_df = pd.DataFrame(columns=['Ticker', 'Sell_Date'])
+
+            # Iterate through buy and sell signals
+            for date, ticker, buy_signal, sell_signal in zip(df.index, ticker_column, buy_column, sell_column):
+                if buy_signal == 1:
+                    buy_date = date  # Update buy_date when a "buy" signal is found
+                    ticker_name = ticker
+                    filtered_buy_df = pd.concat(
+                        [filtered_buy_df, pd.DataFrame({'Ticker': [ticker_name], 'Buy_Date': [buy_date]})],
+                        ignore_index=True)
+                if sell_signal == -1:
+                    sell_date = date  # Update sell_date when a "sell" signal is found
+                    ticker_name = ticker
+                    filtered_sell_df = pd.concat(
+                        [filtered_sell_df, pd.DataFrame({'Ticker': [ticker_name], 'Sell_Date': [sell_date]})],
+                        ignore_index=True)
+
+            # Append the filtered buy and sell DataFrames to their respective lists
+            buy_dataframes.append(filtered_buy_df)
+            sell_dataframes.append(filtered_sell_df)
+
+        def __return_data__(self):
+            """
+            Private method for pulling return data from our finance database.
+
+            Returns:
+                A vector with return data up to a given date and x-periods behind.
+
+            """
+
+        # Now we can create some return vectors, where the end-date point is given by
+        # the last date, which we get from buy_dataframes and sell_dataframes.
+
+        print("k")
+if __name__ == '__main__':
+    instance = Algo2(start_date='2023-01-01',end_date='2023-08-21',tickers_list=['TSLA','AAPL','AMZN'])
+    f = instance.random_forest()
