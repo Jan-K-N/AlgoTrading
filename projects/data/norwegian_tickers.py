cb36977a6c672f0550f2ce6685bd42070b5a633d
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-10-02 09:54:14 +0200

    Tickers and try/except.
    Added try/except in Algo1 so that tickers, which are not found, can be handled in a proper way. Furthermore, I added more Norwegian tickers.

--- projects/data/norwegian_tickers.py
+++ projects/data/norwegian_tickers.py
@@ -31,18 +31,40 @@ class TickerCodeProvider:
         Returns:
             List[str]: A list of ticker codes.
         """
-        ticker_codes_additional = ["PGS.OL","ADE.OL","HAFNI.OL","KAHOT.OL",
-                                   "VAR.OL","SOFF.OL","MPCC.OL","GOGL.OL",
-                                   "KOG.OL","NOD.OL","AKSO.OL","NAS.OL",
-                                   "SHLF.OL","CRAYN.OL","BORR.OL","BWLPG.OL",
-                                   "ELK.OL","DOFG.OL","SRBNK.OL","SCHB.OL",
-                                   "OET.OL","SNI.OL","SDRL.OL","NYKD.OL",
-                                   "OKEA.OL","FLNG.OL","AKH.OL","SCATC.OL",
-                                   "NSKOG.OL","ELMRA.OL","HEX.OL","HAUTO.OL",
-                                   "HAUTO.OL","AUSS.OL","EPR.OL","ODL.OL",
-                                   "AUTO.OL","QFR.OL","BRG.OL","LINK.OL",
-                                   "AGAS.OL","ULTI.OL","GSF.OL","PEN.OL",
-                                   "PROT.OL","MING.OL","RECSI.OL","VEI.OL",
-                                   "NEXT.OL","WAWI.OL","PHO.OL","KIT.CO"
-        ]
+        ticker_codes_additional = ["EQNR.OL", "AKRBP.OL", "FRO.OL", "DNB.OL", "PGS.OL",
+                                   "NHY.OL", "DNO.OL", "DOFG.OL", "TGS.OL", "SOFF.OL", "MOWI.OL", "KOG.OL", "YAR.OL",
+                                   "ZAP.OL", "SALM.OL", "TOM.OL", "TEL.OL", "AKSO.OL", "MPCC.OL", "HAFNI.OL", "SDRL.OL",
+                                   "VAR.OL", "NAS.OL", "NEL.OL", "SHLF.OL", "SNI.OL", "ORK.OL", "SUBC.OL", "NOD.OL",
+                                   "NSKOG.OL", "HKY.OL", "BAKKA.OL", "KAHOT.OL",
+                                   "ELMRA.OL", "GOGL.OL", "ARGEO.OL",
+                                   "RECSI.OL", "CRAYN.OL", "AKH.OL", "GJF.OL",
+                                   "GCC.OL", "STB.OL", "ELK.OL", "EPR.OL",
+                                   "BORR.OL", "KIT.OL", "ULTI.OL", "LINK.OL",
+                                   "ADE.OL", "LSG.OL", "OKEA.OL", "SCHA.OL",
+                                   "SALME.OL", "SRBNK.OL", "BWLPG.OL", "BWO.OL", "AMSC.OL", "CLCO.OL", "AKER.OL",
+                                   "NORAM.OL", "PHO.OL", "ODL.OL", "BONHR.OL", "KCC.OL", "HPUR.OL", "NOM.OL", "VOW.OL",
+                                   "2020.OL", "HAUTO.OL", "PNOR.OL", "OTL.OL", "HEX.OL", "AGAS.OL", "SCATC.OL",
+                                   "OET.OL", "PEN.OL", "PROT.OL", "NODL.OL", "NONG.OL", "WAWI.OL", "LIFE.OL",
+                                   "NORSE.OL", "SBX.OL", "STSU.OL", "SIOFF.OL", "AUSS.OL", "DDRIL.OL", "BRG.OL",
+                                   "SCHB.OL", "SDNS.OL", "ENTRA.OL", "GSF.OL", "SVEG.OL", "MING.OL", "IDEX.OL",
+                                   "VEI.OL", "NYKD.OL", "PRS.OL", "ENSU.OL", "HYPRO.OL","ARCH.OL", "ACC.OL", "KOA.OL",
+                                   "CLOUD.OL", "SEAPT.OL", "TRMED.OL", "PARB.OL", "NEXT.OL",
+                                   "AGLX.OL", "EIOF.OL", "WEST.OL",
+                                   "CADLR.OL", "BELCO.OL", "AFK.OL", "AUTO.OL", "GIG.OL", "AKAST.OL", "PLT.OL",
+                                   "BGBIO.OL", "SSG.OL", "FLNG.OL", "SDSD.OL", "ODF.OL", "WPU.OL", "VGM.OL", "MGN.OL",
+                                   "ALT.OL", "BNOR.OL", "SASNO.OL", "HSHP.OL", "HAVI.OL", "MVE.OL", "DVD.OL",
+                                   "TECH.OL", "TIETO.OL", "SBO.OL", "WWI.OL", "BWE.OL", "DSRT.OL", "5PG.OL",
+                                   "ODFB.OL", "EWIND.OL", "ATEA.OL", "AZT.OL", "ITERA.OL", "AFG.OL", "BOUV.OL",
+                                   "VOLUE.OL", "RANA.OL", "GEOS.OL", "ASA.OL", "NAVA.OL", "OTOVO.OL",
+                                   "ALNG.OL", "SATS.OL", "BOR.OL", "REACH.OL", "MNTR.OL", "BCS.OL", "QFR.OL",
+                                   "WSTEP.OL", "NKR.OL", "ASTK.OL", "NTI.OL", "ENDUR.OL", "RCR.OL", "MOBA.OL",
+                                   "PEXIP.OL", "PCIB.OL", "XXL.OL", "SIKRI.OL", "OLT.OL", "BMA.OL", "LYTIX.OL",
+                                   "CAMBI.OL", "CODE.OL", "NORBT.OL", "EMGS.OL", "BWIDL.OL", "BEWI.OL", "MAS.OL",
+                                   "ELIMP.OL", "KID.OL", "KMCP.OL", "ELABS.OL", "SPOL.OL", "VISTN.OL", "EFUEL.OL",
+                                   "CARA.OL", "CONTX.OL", "ZAL.OL", "GOD.OL", "ARR.OL", "ASTRO.OL", "CRNA.OL",
+                                   "HMONY.OL", "MORG.OL", "ANDF.OL", "MVW.OL", "STRO.OL"]
+
+
+
+
         return ticker_codes_additional
