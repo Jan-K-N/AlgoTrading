0eb460e558b4d73c8a05acf9a4ef4b933cd6776d
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-10-11 11:12:09 +0200

    Update random_forrest.py
    Linter.

--- projects/models/random_forrest.py
+++ projects/models/random_forrest.py
@@ -8,18 +8,57 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 import numpy as np
-import pandas as pd
-
+# pylint: disable=too-many-locals
+# pylint: disable=too-few-public-methods
 class RandomForrest:
+    """
+    A class for training a RandomForestRegressor model and making predictions.
+
+    Parameters:
+        series (numpy.ndarray): The target series we want to predict.
+        x_data (numpy.ndarray): Predictors.
+
+    Attributes:
+        series (numpy.ndarray): The target series.
+        x_data (numpy.ndarray): Predictors.
+
+    Methods:
+        predictor(out_of_sample=True):
+            Trains a RandomForestRegressor model, evaluates its performance, and makes predictions.
+
+            Parameters:
+                out_of_sample (bool): If True, makes out-of-sample predictions. Default is True.
+
+            Returns:
+                tuple: A tuple containing:
+                    - evaluation_list (list): A list of dictionaries containing evaluation metrics.
+                    - best_model: The trained model with the best hyperparameters.
+                    - y_out_of_sample_pred: Predictions for out-of-sample data.
+    """
     def __init__(self,series=None,x_data=None):
         """
-        series: The target series we want to predict.
-        x_data: predictors.
+        Initializes a RandomForrest instance.
+
+        Parameters:
+            series (numpy.ndarray): The target series we want to predict.
+            x_data (numpy.ndarray): Predictors.
         """
         self.series = series
         self.x_data=x_data
 
     def predictor(self, out_of_sample=True):
+        """
+        Trains a RandomForestRegressor model, evaluates its performance, and makes predictions.
+
+        Parameters:
+            out_of_sample (bool): If True, makes out-of-sample predictions. Default is True.
+
+        Returns:
+            tuple: A tuple containing:
+                - evaluation_list (list): A list of dictionaries containing evaluation metrics.
+                - best_model: The trained RandomForestRegressor model with the best hyperparameters.
+                - y_out_of_sample_pred: Predictions for out-of-sample data.
+        """
         evaluation_list = []
 
         common_regr_params = {
@@ -27,7 +66,10 @@ class RandomForrest:
         }
 
         series_final = self.series.reshape(-1, 1)
-        X_train, X_test, y_train, y_test = train_test_split(series_final, self.x_data, test_size=0.25, random_state=42)
+        x_train, x_test, y_train, y_test = train_test_split(series_final,
+                                                            self.x_data,
+                                                            test_size=0.25,
+                                                            random_state=42)
 
         # Model training:
         param_grid = {
@@ -37,7 +79,7 @@ class RandomForrest:
         grid_search = GridSearchCV(estimator=RandomForestRegressor(**common_regr_params),
                                    param_grid=param_grid, cv=5)
         # Fit the model:
-        grid_search.fit(X_train, y_train)
+        grid_search.fit(x_train, y_train)
 
         # Access the best hyperparameters
         best_params = grid_search.best_params_
@@ -50,10 +92,10 @@ class RandomForrest:
         )
 
         # Retrain the model with the best hyperparameters
-        best_regr.fit(X_train, y_train)
+        best_regr.fit(x_train, y_train)
 
         # Evaluate the best model on the test set
-        y_test_pred = best_regr.predict(X_test)
+        y_test_pred = best_regr.predict(x_test)
 
         # Calculate evaluation metrics:
         mae = mean_absolute_error(y_test, y_test_pred)
@@ -65,21 +107,9 @@ class RandomForrest:
 
         # Choose the best model based on the lowest RMSE
         best_model_index = np.argmin([result['RMSE'] for result in evaluation_list])
-        best_model = best_regr if best_model_index == 0 else None  # In this case, we have only one model
+        best_model = best_regr if best_model_index == 0 else None
 
         # Use the best model for out-of-sample forecasting
         if out_of_sample:
-            # Ensure that the format of series_final[-1] matches the input features used during training
             y_out_of_sample_pred = best_model.predict(series_final[-1].reshape(1, -1))
         return evaluation_list, best_model, y_out_of_sample_pred
-
-if __name__ == "__main__":
-    # Set a random seed for reproducibility
-    np.random.seed(42)
-
-    # Generate a random returns series
-    num_days = 100
-    returns = np.random.normal(loc=0.0005, scale=0.01, size=num_days)
-    instance = RandomForrest(series=returns[:-1],x_data=returns[1:])
-    k = instance.predictor()
-    print("k")
