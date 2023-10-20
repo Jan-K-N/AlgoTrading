0bb93e5d68e3cc0ad2f175131451a706945be360
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-10-20 15:36:33 +0200

    Update algo2.py
    Algo2 import-error linter.

--- projects/algos/algo2.py
+++ projects/algos/algo2.py
@@ -14,7 +14,7 @@ by this specific algorithm.
 # pylint: disable=inconsistent-return-statements
 # pylint: disable=too-many-branches
 # pylint: disable=too-many-statements
-import sys
+
 from datetime import datetime, timedelta
 from typing import Union, Optional
 
@@ -23,12 +23,8 @@ import pandas as pd
 from statsmodels.stats.proportion import proportion_confint
 from statsmodels.tsa.arima.model import ARIMA
 
-# Ensure that the paths are inserted before importing local modules
-sys.path.insert(1, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/data')
-sys.path.insert(2, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/models')
-
-from finance_database import Database
-from random_forrest import RandomForrest
+from data.finance_database import Database
+from models.random_forrest import RandomForrest
 from algo1 import Algo1
 
 class Algo2:
