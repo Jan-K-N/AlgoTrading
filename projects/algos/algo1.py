912efaca4aae567a5ffbfe3014decab7cf71ab08
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-10-23 15:59:29 +0200

    Update of algo1 and frontend.
    I fixed the issues with the consecutive_days input in the dash-app. In the next commit, we need to change/insert a consecutive_days_sell argument, so that we sell as soon/from day 1 the sell signals are met. In that way we don't wait. This could also be a variable, so that we can change it if we want to be more confident in our short positions.

--- projects/algos/algo1.py
+++ projects/algos/algo1.py
@@ -1,8 +1,6 @@
 """
 Main script for the Algo1.
 """
-# pylint: disable=import-error
-# pylint: disable=wrong-import-position
 import sys
 sys.path.insert(1, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/strategies')
 from bb import BollingerBandsStrategy
@@ -10,88 +8,32 @@ from rsi import RSIStrategy
 sys.path.insert(2, '/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/strategies')
 from finance_database import Database
 import pandas as pd
+import numpy as np
 
 class Algo1:
-    """
-    A class representing an algorithm for generating trading signals based on
-    Relative Strength Index (RSI) and Bollinger Bands strategies.
-
-    Attributes:
-    -----------
-    ticker : str
-        The ticker symbol of the financial instrument.
-    start_date : str or None
-        The start date for retrieving price data. If None, the default is None.
-    end_date : str or None
-        The end date for retrieving price data. If None, the default is None.
-    tickers_list : list or None
-        A list of ticker symbols for multiple financial instruments. If None, the default is None.
-
-    Methods:
-    --------
-    rsi() -> pd.Series:
-        Calculates the Relative Strength Index (RSI) for the specified ticker and date range.
-
-    bollinger_bands() -> pd.DataFrame:
-        Retrieves Bollinger Bands data for the specified ticker and date range.
-
-    generate_signals() -> pd.DataFrame:
-        Generates buy and sell signals based on RSI and Bollinger Bands strategies.
-
-    algo1_loop() -> list:
-        Executes the algorithm for multiple tickers. The function returns a
-        single DataFrame with the buying/selling signals.
-    """
-    def __init__(self,ticker=None, start_date=None,end_date=None, tickers_list=None, consecutive_days=2):
+    def __init__(self, ticker=None, start_date=None, end_date=None, tickers_list=None, consecutive_days=None):
         self.ticker = ticker
         self.start_date = start_date
         self.end_date = end_date
         self.tickers_list = tickers_list
         self.consecutive_days = consecutive_days
-    def rsi(self)->pd.Series:
-        """
-        Calculates the Relative Strength Index (RSI) for the specified ticker and date range.
 
-        Returns:
-        -------
-        rsi_data : pd.Series
-            Series containing the RSI values.
-        """
-        rsi_instance = RSIStrategy(ticker = self.ticker,
-                                   start_date=self.start_date,
-                                   end_date=self.end_date)
+    def rsi(self) -> pd.Series:
+        rsi_instance = RSIStrategy(ticker=self.ticker, start_date=self.start_date, end_date=self.end_date)
         rsi_data = rsi_instance.get_data()['RSI']
         return rsi_data
-    def bollinger_bands(self):
-        """
-        Retrieves Bollinger Bands data for the specified ticker and date range.
 
-        Returns:
-        -------
-        bollingerbands_data : pd.DataFrame
-            DataFrame containing the Bollinger Bands data.
-        """
-        bollingerbands_instance = BollingerBandsStrategy(ticker = self.ticker,
-                                                         start_date=self.start_date,
-                                                         end_date=self.end_date)
+    def bollinger_bands(self):
+        bollingerbands_instance = BollingerBandsStrategy(ticker=self.ticker, start_date=self.start_date, end_date=self.end_date)
         bollingerbands_data = bollingerbands_instance.get_data()
         return bollingerbands_data
 
     def generate_signals(self):
-        """
-        Generates buy and sell signals based on RSI and Bollinger Bands strategies.
+        data = Database.get_price_data(self, ticker=self.ticker, start=self.start_date, end=self.end_date)['Adj Close']
 
-        Returns:
-        signals : pd.DataFrame
-            DataFrame containing the buy and sell signals.
-        """
-        data = Database.get_price_data(self, ticker=self.ticker,
-                                       start=self.start_date,
-                                       end=self.end_date)['Adj Close']
-
-        lower_band = Algo1.bollinger_bands(self)['Lower']
-        upper_band = Algo1.bollinger_bands(self)['Upper']
-        rsi = Algo1.rsi(self)
+        lower_band = self.bollinger_bands()['Lower']
+        upper_band = self.bollinger_bands()['Upper']
+        rsi = self.rsi()
 
         current_price = data.values
 
@@ -101,22 +43,28 @@ class Algo1:
 
         consecutive_buy = 0
         consecutive_sell = 0
-        buy_signal = []
-        sell_signal = []
+        buy_signal = [0] * len(data)
+        sell_signal = [0] * len(data)
 
         for i in range(len(data)):
-            if (rsi_aligned[i] < 30) and (current_price[i] < lower_band_aligned[i]):
-                consecutive_buy += 1
-                consecutive_sell = 0
-            elif (rsi_aligned[i] > 70) and (current_price[i] > upper_band_aligned[i]):
-                consecutive_sell += 1
-                consecutive_buy = 0
+            if not np.isnan(rsi_aligned[i]) and not np.isnan(current_price[i]) and not np.isnan(lower_band_aligned[i]):
+                if (rsi_aligned[i] < 30) and (current_price[i] < lower_band_aligned[i]):
+                    consecutive_buy += 1
+                    consecutive_sell = 0
+                elif (rsi_aligned[i] > 70) and (current_price[i] > upper_band_aligned[i]):
+                    consecutive_sell += 1
+                    consecutive_buy = 0
+                else:
+                    consecutive_buy = 0
+                    consecutive_sell = 0
+
+                buy_signal[i] = 1 if (
+                            self.consecutive_days is not None and consecutive_buy >= self.consecutive_days) else 0
+                sell_signal[i] = -1 if (
+                            self.consecutive_days is not None and consecutive_sell >= self.consecutive_days) else 0
             else:
                 consecutive_buy = 0
                 consecutive_sell = 0
-
-            buy_signal.append(1 if consecutive_buy >= self.consecutive_days else 0)  # Use self.consecutive_days
-            sell_signal.append(-1 if consecutive_sell >= self.consecutive_days else 0)  # Use self.consecutive_days
 
         signals = pd.DataFrame(data.index, columns=['Date'])
         signals[self.ticker + '_Buy'] = buy_signal
@@ -124,27 +72,12 @@ class Algo1:
 
         return signals
 
-    def algo1_loop(self)->list:
-        """
-        Executes the algorithm for multiple tickers and generates
-        buying/selling signals for each ticker.
-
-        Returns:
-        -------
-        list of pandas.DataFrame:
-        A list of pandas DataFrames, each containing the buying/selling signals for a ticker.
-        Each DataFrame has 3 columns: 'Date', 'Buy', and 'Sell', where 'Buy' and 'Sell'
-        are binary indicators of whether a buy or a sell signal was generated on that
-        date for the corresponding ticker.
-        """
-
+    def algo1_loop(self) -> list:
         signals_list = []
 
         for ticker1 in self.tickers_list:
             try:
-                instance_1 = Algo1(ticker=ticker1,
-                                   start_date=self.start_date,
-                                   end_date=self.end_date)
+                instance_1 = Algo1(ticker=ticker1, start_date=self.start_date, end_date=self.end_date)
                 signals_1 = instance_1.generate_signals()
             except KeyError as error:
                 print(f"KeyError for {ticker1}: {str(error)}")
@@ -169,3 +102,9 @@ class Algo1:
             if not new_df.empty:
                 signals_list.append(new_df)
         return signals_list
+
+
+
+
+
+
