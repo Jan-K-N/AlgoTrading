389722faf4d691f9cb460afd64b836205b50532c
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-11-22 12:39:45 +0100

    Update views.py
    Linter.

--- projects/website/mywebsite/myapp/views.py
+++ projects/website/mywebsite/myapp/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to My Website")
+    return HttpResponse("Welcome to My Website.")
 
 def about(request):
     return HttpResponse("About Page")
