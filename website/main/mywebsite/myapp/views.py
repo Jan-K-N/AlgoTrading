f010d34cab0b79157b18498955b59ca7400b6764
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-11-22 11:06:56 +0100

    Init commit of the new django app.
    Created the app, and made a toy website.

---
+++ website/main/mywebsite/myapp/views.py
@@ -0,0 +1,8 @@
+# myapp/views.py
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to My Website")
+
+def about(request):
+    return HttpResponse("About Page")
