dd72c1059fa7ae836773b2bf5bbce5f086871361
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-12-01 14:02:55 +0100

    Addition of html file.
    Added a html file, which should contain the newsfeed. Furthermore, I did some refactoring.

--- projects/website/mywebsite/myapp/views.py
+++
@@ -1,8 +0,0 @@
-# myapp/views.py
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to My Website.")
-
-def about(request):
-    return HttpResponse("About Page")
