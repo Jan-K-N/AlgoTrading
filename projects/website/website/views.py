a61e1054803a37a0232f306aa83d5fca5113e505
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-12-05 14:04:22 +0100

    Static signals table.
    Added a static signals table to the home page. In the this commit, this needs to be dynamically.

--- projects/website/website/views.py
+++ projects/website/website/views.py
@@ -8,8 +8,18 @@ def home(request):
     # You can add your news content here later
     news_content = []
 
+
+
+    signals_data = [
+        {'ticker': 'AAPL', 'signal': 'Buy', 'price': 150.25, 'date': '2023-12-01'},
+        {'ticker': 'GOOGL', 'signal': 'Sell', 'price': 2800.50, 'date': '2023-12-02'},
+        # Add more signals as needed
+    ]
+
     # Pass the news content to the template
-    context = {'news_content': news_content}
+    context = {'news_content': news_content,
+               'signals_data':signals_data}
+
     return render(request, 'myapp/home.html', context)
 
 # def about(request):
