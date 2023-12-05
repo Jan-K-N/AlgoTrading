04fb36660ef8540549d869945bbb071ff1045312
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-12-05 14:47:42 +0100

    Update.
    Tried to import Algo1 into the views.

--- projects/website/website/views.py
+++ projects/website/website/views.py
@@ -1,14 +1,19 @@
 # myapp/views.py
 from django.http import HttpResponse
 from datetime import timedelta, date
+import sys
+sys.path.append("/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/algos")
 from django.shortcuts import render
-
+from algo1 import Algo1
 
 def home(request):
     # You can add your news content here later
     news_content = []
 
-
+    instance = Algo1(start_date = "2021-01-01",end_date="2023-01-01",
+                     tickers_list=['TSLA','AAPL'])
+    import pdb; pdb.set_trace()
+    k = instance.algo1_loop()
 
     signals_data = [
         {'ticker': 'AAPL', 'signal': 'Buy', 'price': 150.25, 'date': '2023-12-01'},
@@ -22,7 +27,5 @@ def home(request):
 
     return render(request, 'myapp/home.html', context)
 
-# def about(request):
-#     return HttpResponse("About Page")
 def about(request):
     return render(request, 'myapp/about.html')
