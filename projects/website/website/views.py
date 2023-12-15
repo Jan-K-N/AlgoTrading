85f7c42bda7df6809929bd3a3e507ed03f796200
Jan-K-N <66866576+Jan-K-N@users.noreply.github.com>
2023-12-15 16:22:08 +0100

    Views and home update
    Added debug statements to home.html. Furthermore, I fixed import issues in views. Next step is to insert algo1 format into home.html.

--- projects/website/website/views.py
+++ projects/website/website/views.py
@@ -1,29 +1,39 @@
 # myapp/views.py
 from django.http import HttpResponse
 from datetime import timedelta, date
+
 import sys
+print(sys.path)
 sys.path.append("/Users/Jan/Desktop/Programmering/StocksAlgo/AlgoTrading/projects/algos")
+import pandas
+
 from django.shortcuts import render
 from algo1 import Algo1
+
+instance = Algo1(start_date="2021-01-01", end_date="2023-01-01", consecutive_days = 1,
+                 ticker = 'TSLA')
+
+
 
 def home(request):
     # You can add your news content here later
     news_content = []
 
-    instance = Algo1(start_date = "2021-01-01",end_date="2023-01-01",
-                     tickers_list=['TSLA','AAPL'])
-    import pdb; pdb.set_trace()
-    k = instance.algo1_loop()
+    # instance = Algo1(start_date = "2021-01-01",end_date="2023-01-01",
+    #                  tickers_list=['TSLA','AAPL','FLS.CO'])
+    # import pdb; pdb.set_trace()
+    # k = instance.algo1_loop()
 
     signals_data = [
-        {'ticker': 'AAPL', 'signal': 'Buy', 'price': 150.25, 'date': '2023-12-01'},
+        {'ticker': 'AAPL', 'signal': 'Buy', 'price': 165.25, 'date': '2023-12-01'},
         {'ticker': 'GOOGL', 'signal': 'Sell', 'price': 2800.50, 'date': '2023-12-02'},
         # Add more signals as needed
     ]
 
     # Pass the news content to the template
     context = {'news_content': news_content,
-               'signals_data':signals_data}
+               'signals_data':signals_data
+               }
 
     return render(request, 'myapp/home.html', context)
 
