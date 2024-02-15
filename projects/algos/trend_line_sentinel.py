2d9f38ebd6b8608f5ab4a251ed427be5d885802c
Jan Kindt Nielsen <66866576+Jan-K-N@users.noreply.github.com>
2024-02-15 13:37:02 +0100

    Update trend_line_sentinel.py

--- projects/algos/trend_line_sentinel.py
+++ projects/algos/trend_line_sentinel.py
@@ -12,17 +12,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("..")
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
+
 from data.finance_database import Database
+
+from algo_scrapers.s_and_p_scraper import SAndPScraper
+
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.neural_network import MLPRegressor
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from statsmodels.tsa.seasonal import seasonal_decompose
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import Lasso
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
-from algo_scrapers.s_and_p_scraper import SAndPScraper
+
 
 class sentinel:
 
@@ -70,25 +76,31 @@ class sentinel:
         return sentinel_features
     def generate_signals(self):
         """
-        Method for generating signals based on a linear regression method.
-        Signals are made in the following way:
+        Method for generating trading signals using a neural network model.
 
-            - Buy:
-                When the current/latest price of the stock is above the regression line,
-                we generate a buy signal, indicating that the stock is
-                potentially undervalued or trending upwards.
-            - Sell:
-                When the current price is below the regression line,
-                we generate a sell signal, suggesting that the stock may
-                be overvalued or trending downwards.
+        Signals are generated based on the forecasted stock prices
+        obtained from the neural network model.
 
-        Trading Orders: Using these buy and sell signals, we generate trading orders:
-        When transitioning from a sell signal to a buy signal, we place a buy order, indicating a long position.
-        When transitioning from a buy signal to a sell signal, we place a sell order, indicating a short position.
+        Buy Signal:
+        When the forecasted price is above the current/latest price
+        of the stock, a buy signal is generated.
+        This indicates that the stock may be undervalued or trending upwards.
+
+        Sell Signal:
+        When the forecasted price is below the current/latest price of the stock,
+        a sell signal is generated.
+        This suggests that the stock may be overvalued or trending downwards.
+
+        Trading Orders:
+        - Buy Order: Generated when transitioning from a sell signal to a buy signal, indicating a long position.
+        - Sell Order: Generated when transitioning from a buy signal to a sell signal, indicating a short position.
 
         Returns:
-
+        DataFrame: A DataFrame containing the trading signals and corresponding positions.
+        The 'signal' column indicates the trading signal (+1 for buy, -1 for sell), and the 'positions' column
+        specifies the position to take (1 for long, -1 for short).
         """
+
         data = self.sentinel_data()
         signals = pd.DataFrame(index=data.index)
         signals['signal'] = 0.0
@@ -109,58 +121,40 @@ class sentinel:
 
         data2 = self.sentinel_features_data()
 
-
         X = np.column_stack((x, seasonal_sin, seasonal_cos, seasonal_scaled, data2))
 
         # Split the data into training and test sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        # Create a pipeline for Lasso regression
-        lasso_pipeline = Pipeline([
-            ('imputer', SimpleImputer(strategy='mean')), # Impute missing values by mean.
+        # Create a pipeline for neural network regression
+        nn_pipeline = Pipeline([
+            ('imputer', SimpleImputer(strategy='mean')),  # Impute missing values by mean.
             ('scaler', StandardScaler()),
-            ('lasso', Lasso(alpha=0.1))
-        ])
-
-        # Fit Lasso regression
-        lasso_pipeline.fit(X_train, y_train)
-
-        # Transform the features using Lasso
-        X_test_lasso = lasso_pipeline['imputer'].transform(X_test)
-        X_test_lasso = lasso_pipeline['scaler'].transform(X_test)
-        X_train_lasso = lasso_pipeline.fit_transform(X_train, y_train)
-
-        # Create a pipeline for Gradient Boosting Regressor
-        gbr_pipeline = Pipeline([
-            ('scaler', StandardScaler()),
-            ('gbr', GradientBoostingRegressor())
+            ('mlp', MLPRegressor())
         ])
 
         # Define hyperparameters grid for GridSearchCV
         param_grid = {
-            'gbr__n_estimators': [50, 100, 300],  # Adjust number of boosting stages
-            'gbr__max_depth': [3, 4, 5],  # Adjust maximum depth of individual trees
-            'gbr__learning_rate': [0.01, 0.1, 0.2]  # Adjust learning rate
+            'mlp__hidden_layer_sizes': [(50,), (100,), (50, 50)],  # Adjust number of hidden layers and units
+            'mlp__activation': ['relu', 'tanh'],  # Activation function options
+            'mlp__alpha': [0.0001, 0.001, 0.01],  # Regularization parameter
         }
 
         # Perform grid search with cross-validation
-        grid_search = GridSearchCV(gbr_pipeline, param_grid, cv=10, scoring='neg_mean_squared_error')
-        grid_search.fit(X_train_lasso, y_train)
-
-        # Transform the test data using the same scaler
-        X_test_lasso_scaled = lasso_pipeline['scaler'].transform(X_test_lasso)
+        grid_search = GridSearchCV(nn_pipeline, param_grid, cv=10, scoring='neg_mean_squared_error')
+        grid_search.fit(X_train, y_train)
 
         # Get the best model from grid search
         best_model = grid_search.best_estimator_
 
         # Make predictions on the test set
-        y_pred = best_model.predict(X_test_lasso_scaled)
+        y_pred = best_model.predict(X_test)
 
         # Compute RMSE
         rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
         # Set threshold
-        threshold = 5  # Define your threshold value
+        threshold = 10  # Define your threshold value
 
         # Use the forecast if RMSE is below the threshold
         if rmse <= threshold:
@@ -182,7 +176,6 @@ class sentinel:
 
         else:
             print("Forecast RMSE is above the threshold. Discarding the forecast.")
-            # Handle the case where the forecast does not meet the threshold
 
         return signals
 
@@ -196,10 +189,6 @@ class sentinel:
 
         signals = self.generate_signals()
 
-        # # Plotting fitted values, dropping the first observation
-        # ax.plot(data.index[1:], signals['regression_line'].iloc[1:], label='Fitted Values', linestyle='--',
-        #         color='orange')
-
         # Plotting buy signals
         buy_indices = signals[signals['signal'] == 1.0].index
         buy_values = data[self.ticker][signals['signal'] == 1.0]
@@ -210,61 +199,14 @@ class sentinel:
         sell_values = data[self.ticker][signals['signal'] == -1.0]
         ax.plot(sell_indices, sell_values, 'v', markersize=10, color='r', label='Sell Signal')
 
-        # ax.set_title('Linear Regression Trading Strategy')
+        ax.set_title('NN Trading Strategy')
         ax.set_xlabel('Date')
         ax.set_ylabel('Price')
         ax.legend()
 
-        plt.show()
+        plot = plt.show()
 
-    def backtest(self):
-        """
-        Method for backtesting the trading strategy.
-        Returns:
-            dict: A dictionary containing backtest results.
-        """
-        # Generate signals
-        signals = self.generate_signals()
-
-        # Initialize variables
-        initial_cash = 10000  # Initial cash amount
-        cash = initial_cash
-        position = 0  # Initial position (no position)
-        last_signal = 0  # Initial signal (no signal)
-        positions = []  # Store positions for analysis
-
-        # Iterate over signals and calculate returns
-        for index, signal in signals.iterrows():
-            if signal['positions'] == 1 and last_signal != 1:  # Buy signal
-                shares_bought = cash / signals.loc[index, self.ticker]
-                position = shares_bought
-                cash = 0
-                last_signal = 1
-                positions.append((index, 'Buy', signals.loc[index, self.ticker], position, cash))
-            elif signal['positions'] == -1 and last_signal != -1:  # Sell signal
-                cash = position * signals.loc[index, self.ticker]
-                position = 0
-                last_signal = -1
-                positions.append((index, 'Sell', signals.loc[index, self.ticker], position, cash))
-
-        # Calculate final value
-        final_value = cash + position * signals.iloc[-1][self.ticker]
-
-        # Calculate return
-        returns = (final_value - initial_cash) / initial_cash * 100
-
-        # Create a dictionary to store backtest results
-        backtest_results = {
-            'Initial Cash': initial_cash,
-            'Final Value': final_value,
-            'Returns (%)': returns,
-            'Positions': positions
-        }
-
-        return backtest_results
-
-
-
+        return plot
 
 if __name__ == "__main__":
     instance = sentinel(start_date="2023-06-01",end_date="2024-01-01",
@@ -273,9 +215,10 @@ if __name__ == "__main__":
     k = instance.sentinel_data()
     f = instance.generate_signals()
 
-    # 3. Plot signals on the price chart
-    instance.plot_signals()
+    # # 3. Plot signals on the price chart
+    # instance.plot_signals()
     f1=instance.backtest()
+    print("k")
 
 
 
